41c81169f9e1a1a30d4fda5605bf66b8fc5c2a55
Marsha Hall <mphall77@gmail.com>
2021-06-23 20:05:51 -0400

    css for table

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom";
 const Transaction = ({ transaction, index }) => {
 	return (
 		<tr>
-			<td>{transaction.from}</td>
+			{/* <td>{transaction.from}</td> */}
 			<td>{transaction.date}</td>
-			<td>
-				<Link to={`/transactions/${index}`}>{transaction.name}</Link>
-			</td>
+			<Link to={`/transactions/${index}`}>{transaction.name}</Link>
 			<td>{transaction.amount}</td>
 		</tr>
 	);
--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -1,17 +1,36 @@
 import React from "react";
 import Transaction from "./Transaction";
+// import { useState, useEffect } from "react";
 
 const Transactions = ({ transactions }) => {
+	// const [balance, setBalance] = useState([])
+
+	// const accBalance = ({transactions}) => {
+	// 	let sum = 0;
+	// 	let total = 0;
+	// 	debugger
+	// 	 total =
+	// 		transactions.map(transaction => {
+	// 			sum += transaction.amount;
+	// 			return (sum);
+	// 		})
+	// 	setBalance(total);
+	// }
+
+	// useEffect(() => {
+	// 	accBalance();
+	// }, []);
+
 	return (
 		<section className="Transactions">
 			<h1>Transactions</h1>
+			<div className="Balance">Account Balance: </div>
 			<table>
 				<thead>
 					<tr>
-						<th></th>
-						<th></th>
-						<th>See this transaction</th>
-						<th></th>
+						<th>Date</th>
+						<th>Purchase</th>
+						<th>Amount</th>
 					</tr>
 				</thead>
 				<tbody>
